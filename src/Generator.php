121aa60f555ee5b714c4e2233832979cd342040d
Rémy <baudon.remy@gmail.com>
2020-12-02 10:56:22 +0100

    Fix schema

--- src/Generator.php
+++ src/Generator.php
@@ -17,7 +17,13 @@ class Generator
         $xmlService->namespaceMap = [
             'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2' => '',
             'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2' => 'cbc',
-            'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2' => 'cac'
+            'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2' => 'cac',
+            'urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2' => 'qdt',
+            'urn:oasis:names:specification:ubl:schema:xsd:CoreComponentParameters-2' => 'ccts',
+            'urn:oasis:names:specification:ubl:schema:xsd:DocumentStatusCode-1.0' => 'stat',
+            'urn:un:unece:uncefact:data:draft:UnqualifiedDataTypesSchemaModule:2' => 'udt',
+            'urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2' => 'cec',
+            'urn:AIFE:Facture:Extension' => 'aife'
         ];
 
         return $xmlService->write('Invoice', [
